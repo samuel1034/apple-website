d0e5e084f64bef3ee039cb30ac9bf197c775abbf
Samuel Fuentes (Genius) <samuelgenius1034@gmail.com>
2024-03-31 17:04:59 -0400

    First Commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <section className='w-full nav-height bg-black relative'>
+      <div className='h-5/6 w-full flex-center flex-col'>
+        <p> Iphone 17 Pro</p>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
